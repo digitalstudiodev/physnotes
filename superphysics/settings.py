51b207c3865c23864769f10f7c94bcb07a923a40
jose-dom <josedom000@gmail.com>
2020-12-29 11:37:10 -0500

    update

--- superphysics/settings.py
+++ superphysics/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['https://physicsrepo.herokuapp.com/']
+ALLOWED_HOSTS = ['https://physicsrepo.herokuapp.com/', 'www.physnotes.net/']
 
 
 # Application definition
