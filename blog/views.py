00462f46c687bd8f34b63473ad1290c6ef19a6ac
Jose Dominguez <josedominguez@ab-man-billw.rad.rutgers.edu>
2021-10-05 11:57:57 -0400

    improve detail view

--- blog/views.py
+++ blog/views.py
@@ -47,7 +47,9 @@ class PostDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comments'] = Comment.objects.filter(post=self.object)
+        comments = Comment.objects.filter(post=self.object)
+        if comments.count > 0:
+            context['comments'] = Comment.objects.filter(post=self.object)
         return context
 
 class PostCreateView(LoginRequiredMixin, CreateView):
