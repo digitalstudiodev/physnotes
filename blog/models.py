32bdd58584f74c9c4bb2bbe9151ba1d63349bfb5
Jose <jrd251@rutgers.edu>
2023-09-26 19:53:42 -0400

    add ground work for rss feeds

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,22 @@ from multiselectfield import MultiSelectField
 from PIL import Image
 from .dict_lib import TAG_OPTIONS
 
+# Content Tag - i.e. Nuclear, Optics, Environmental
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=100, default="General")
+    category = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str("Tag", self.tag_name, "Category", self.category.category_name)
 
+# Data Category - i.e. Physics, Math, Chemistry
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, default="General")
+    
+    def __str__(self):
+        return self.category_name
+
+# Curated Content - Free
 class Post(models.Model):
     title = models.CharField(max_length=100, default="")
     preview = models.CharField(max_length=5000, default="")
@@ -14,16 +29,17 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     read_time = models.IntegerField(default=5, verbose_name="Read Time", help_text="in minutes")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tag = MultiSelectField(max_length=100, choices=TAG_OPTIONS, default="Featured", null=False, blank=False, max_choices=4)
+    tag = models.ForeignKey(Tag, on_delete=models.DO_NOTHING)
     featured_image = models.ImageField(default='default.png', upload_to='blog_pics', verbose_name="Featured Image")
     note = models.FileField(default=None, upload_to='notes', verbose_name="Notes", null=True, blank=True, help_text="(Optional)")
 
     def __str__(self):
-        return self.title
+        return str(self.title, self.date_posted)
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', kwargs={'pk': self.pk})
 
+# User Comment on Curated Content
 class Comment(models.Model):
     user = models.CharField(max_length=100, default="", verbose_name="Name")
     comment = models.TextField(default="", verbose_name="Comment")
@@ -35,3 +51,15 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', kwargs={'pk': self.post.pk})
+    
+# Data from RSS Feeds    
+class RSSPost(models.Model):
+    title = models.CharField(max_length=100, default="")
+    summary = models.CharField(max_length=5000, default="")
+    content = models.TextField(default="", verbose_name="Content")
+    story_link = models.CharField(max_length=5000, default="")
+    media_link = models.CharField(max_length=5000, default="")
+    date_posted = models.DateTimeField(default=None)
+
+    def __str__(self):
+        return str(self.title, self.date_posted)
