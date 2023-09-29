68df14b8d9eacdc5d6c8eb22a33eb811f5f31a98
Jose <jrd251@rutgers.edu>
2023-09-29 17:23:27 -0400

    flex div

--- blog/views.py
+++ blog/views.py
@@ -66,6 +66,11 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'].tags.queryset = Tag.objects.all()
+        return context
+
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
 
