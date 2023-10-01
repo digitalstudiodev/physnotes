1b8b8e6493ddb5638121e74b6ba2d402c02db24c
Jose <jrd251@rutgers.edu>
2023-10-01 12:17:54 -0400

    category view

--- blog/views.py
+++ blog/views.py
@@ -142,6 +142,7 @@ class CategoryListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()
         return context
 
 class CategoryCreateView(LoginRequiredMixin, CreateView):
