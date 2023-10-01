653418a9ebae935396fd44d81fb51d108c841d6e
Jose <jrd251@rutgers.edu>
2023-10-01 14:20:58 -0400

    category view

--- blog/views.py
+++ blog/views.py
@@ -211,8 +211,7 @@ class TagUpdateView(LoginRequiredMixin, UserPassesTestMixin, FormView):
         if self.request.POST:
             # get category keys
             print(self.request.POST)
-            categories, category_objs = self.request.POST['category'], []
-            print(categories)
+            categories, category_objs = list(self.request.POST['category']), []
             # get post object  
             tag_obj = Tag.objects.all().filter(tag_name=self.request.POST['tag_name']).first()
             # use category keys to get list of category objects
